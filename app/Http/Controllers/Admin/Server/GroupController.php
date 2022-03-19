44b892628cdeb0ddbac075ae9c35589f04a70db6
Lisa Admin Github ✔ <93268631+XUwUxX@users.noreply.github.com>
2022-03-19 10:45:08 +0700

    Update GroupController.php

--- app/Http/Controllers/Admin/Server/GroupController.php
+++ app/Http/Controllers/Admin/Server/GroupController.php
@@ -41,7 +41,7 @@ class GroupController extends Controller
     public function save(Request $request)
     {
         if (empty($request->input('name'))) {
-            abort(500, '组名不能为空');
+            abort(500, 'Tên nhóm không được để trống ');
         }
 
         if ($request->input('id')) {
@@ -61,22 +61,22 @@ class GroupController extends Controller
         if ($request->input('id')) {
             $serverGroup = ServerGroup::find($request->input('id'));
             if (!$serverGroup) {
-                abort(500, '组不存在');
+                abort(500, 'Nhóm không tồn tại ');
             }
         }
 
         $servers = ServerV2ray::all();
         foreach ($servers as $server) {
             if (in_array($request->input('id'), $server->group_id)) {
-                abort(500, '该组已被节点所使用，无法删除');
+                abort(500, 'Nhóm đã được sử dụng bởi nút và không thể bị xóa ');
             }
         }
 
         if (Plan::where('group_id', $request->input('id'))->first()) {
-            abort(500, '该组已被订阅所使用，无法删除');
+            abort(500, 'Nhóm đã được đăng ký sử dụng và không thể bị xóa ');
         }
         if (User::where('group_id', $request->input('id'))->first()) {
-            abort(500, '该组已被用户所使用，无法删除');
+            abort(500, 'Nhóm này đã được người dùng sử dụng và không thể bị xóa ');
         }
         return response([
             'data' => $serverGroup->delete()
