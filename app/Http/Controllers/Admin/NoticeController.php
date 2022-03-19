53698e57d89df0d50adc953417950ca9a712a616
Lisa Admin Github ✔ <93268631+XUwUxX@users.noreply.github.com>
2022-03-19 10:17:16 +0700

    Update NoticeController.php

--- app/Http/Controllers/Admin/NoticeController.php
+++ app/Http/Controllers/Admin/NoticeController.php
@@ -26,13 +26,13 @@ class NoticeController extends Controller
         ]);
         if (!$request->input('id')) {
             if (!Notice::create($data)) {
-                abort(500, '保存失败');
+                abort(500, 'Lưu thất bại ');
             }
         } else {
             try {
                 Notice::find($request->input('id'))->update($data);
             } catch (\Exception $e) {
-                abort(500, '保存失败');
+                abort(500, 'Lưu thất bại ');
             }
         }
         return response([
@@ -45,15 +45,15 @@ class NoticeController extends Controller
     public function show(Request $request)
     {
         if (empty($request->input('id'))) {
-            abort(500, '参数有误');
+            abort(500, 'Tham số sai ');
         }
         $notice = Notice::find($request->input('id'));
         if (!$notice) {
-            abort(500, '公告不存在');
+            abort(500, 'Thông báo không tồn tại ');
         }
         $notice->show = $notice->show ? 0 : 1;
         if (!$notice->save()) {
-            abort(500, '保存失败');
+            abort(500, 'Lưu thất bại ');
         }
 
         return response([
@@ -64,14 +64,14 @@ class NoticeController extends Controller
     public function drop(Request $request)
     {
         if (empty($request->input('id'))) {
-            abort(500, '参数错误');
+            abort(500, 'Lỗi tham số ');
         }
         $notice = Notice::find($request->input('id'));
         if (!$notice) {
-            abort(500, '公告不存在');
+            abort(500, 'Thông báo không tồn tại ');
         }
         if (!$notice->delete()) {
-            abort(500, '删除失败');
+            abort(500, 'Không xóa được ');
         }
         return response([
             'data' => true
