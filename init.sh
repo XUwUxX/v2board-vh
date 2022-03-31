b386e26dbe7578663ef76fb70d9989500362a882
Lisa Admin Github ✔ <93268631+XUwUxX@users.noreply.github.com>
2022-03-31 22:14:31 +0700

    Non

--- init.sh
+++
@@ -1,4 +0,0 @@
-rm -rf composer.phar
-wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
-php composer.phar install -vvv
-php artisan v2board:install
--- update_dev.sh
+++
@@ -1,7 +0,0 @@
-git fetch --all && git reset --hard origin/dev && git pull origin dev
-git checkout dev
-rm -rf composer.lock composer.phar
-wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
-php composer.phar update -vvv
-php artisan v2board:update
-php artisan config:cache
